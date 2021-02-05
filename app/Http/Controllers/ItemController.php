eeb8d1814d473b1e413f8df96cc6721ec4743af5
Johan Eka Santosa <43514858+johaneka06@users.noreply.github.com>
2021-02-05 21:00:08 +0700

    Update ItemController.php

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -12,12 +12,6 @@ class ItemController extends Controller
     public function insert(ItemRequest $request)
     {
         $request = $request->validated();
-        
-        // Item::create([
-        //     "name" => $request['name'],
-        //     "price" => $request['price'],
-        //     "stock" => $request['stock']
-        // ]);
 
         $item = new Item;
         $item->name = $request['name'];
@@ -25,10 +19,7 @@ class ItemController extends Controller
         $item->stock = $request['stock'];
         $item->save();
         $item->fresh();
-        
-        
-        dd(new NewItemNotification($item));
-        Mail::to('johanekasantosa@yahoo.com')->send(new NewItemNotification($item));
+                
         return redirect('/');
     }
 
